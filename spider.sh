eb2d5b1bc81e7058a9b97bc1364fa6b64fdf591c
Guillaume Seren <guillaumeseren@gmail.com>
2015-06-17 17:33:54 +0200

    Add getOpts to parse args.

--- spider.sh
+++ spider.sh
@@ -17,6 +17,7 @@
 # 0 - Ok
 
 # Default variables {{{1
+flagGetOpts=0
 
 # FUNCTION usage() {{{1
 # Return the helping message for the use.
@@ -39,3 +40,32 @@ Sample:
 
 DOC
 }
+
+# GETOPTS {{{1
+# Get the param of the script.
+while getopts ":u:h" OPTION
+do
+    flagGetOpts=1
+    case $OPTION in
+    h)
+        usage
+        exit 1
+        ;;
+    u)
+        cmdUrl="$OPTARG"
+        echo "Analyse url:$cmdUrl"
+        ;;
+    ?)
+        echo "commande $1 inconnue"
+        usage
+        exit
+        ;;
+    esac
+done
+# We check if getopts did not find no any param
+if [ "$flagGetOpts" == 0 ]; then
+    echo 'This script cannot be launched without options.'
+    usage
+    exit 1
+fi
+
