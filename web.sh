652a7b985fed1c134349e427670a40351fb2f0ce
Guillaume Seren <guillaumeseren@gmail.com>
2017-05-09 22:38:16 +0200

    Cleaning of the scripts

--- web.sh
+++ web.sh
@@ -41,86 +41,87 @@ DOC
 # Get the param of the script.
 while getopts "l:d:u:h" OPTION
 do
-    flagGetOpts=1
-    case $OPTION in
+  flagGetOpts=1
+  case $OPTION in
     h)
-        usage
-        exit 1
-        ;;
+      usage
+      exit 1
+      ;;
     u)
-        cmdUrl="$OPTARG"
-        ;;
+      cmdUrl="$OPTARG"
+      ;;
     l)
-        cmdLevel="$OPTARG"
-        ;;
+      cmdLevel="$OPTARG"
+      ;;
     d)
-        cmdDomain="$OPTARG"
-        ;;
+      cmdDomain="$OPTARG"
+      ;;
     ?)
-        echo "commande $1 inconnue"
-        usage
-        exit
-        ;;
-    esac
+      echo "commande $1 inconnue"
+      usage
+      exit
+      ;;
+  esac
 done
 # We check if getopts did not find no any param
 if [ "$flagGetOpts" == 0 ]; then
-    echo 'This script cannot be launched without options.'
-    usage
-    exit 1
+  echo 'This script cannot be launched without options.'
+  usage
+  exit 1
 fi
 
 # getLinkFromUrl() {{{1
 function getLinkFromUrl() {
-    if [[ -n "$1" && "$1" != "" ]]; then
-        tempFile="$(mktemp -d)"
-        urlArray=($(wget --spider --force-html --page-requisites -P "$tempFile" -r "$rLevel" "$domainTarget" "$1" 2>&1 | grep '^--' | awk '{ print $3 }' | uniq))
-        rm -r "$tempFile"
+  if [[ -n "$1" ]]; then
+    tempFile="$(mktemp -d)"
+    urlArray=($(wget --spider --force-html --page-requisites -P "$tempFile" -r "$rLevel" "$domainTarget" "$1" 2>&1 | grep '^--' | awk '{ print $3 }' | uniq))
+    rm -r "$tempFile"
 
-    else
-        urlArray=()
-    fi
+  else
+    urlArray=()
+  fi
 }
 
 # getRecursivityLevel() {{{1
 function getRecursivityLevel(){
-    if [[ -n "$1" && "$1" != "" ]]; then
-        if [[ "$1" != 0 ]]; then
-            local rLevel="-l $1"
-        else
-            local rLevel=""
-        fi
+  local rLevel=''
+  if [[ -n "$1" ]]; then
+    if [[ "$1" != 0 ]]; then
+      rLevel="-l $1"
     else
-        local rLevel="-l 1"
+      rLevel=""
     fi
-    echo "$rLevel"
+  else
+    rLevel="-l 1"
+  fi
+  echo "$rLevel"
 }
 
 # getDomainTarget() {{{1
 function getDomainTarget() {
-    local domains=""
-    if [[ -n "$1" && "$1" != "" ]]; then
-            domains="--domains=$1"
-    else
-            domains=""
-    fi
-    echo "$domains"
+  local domains=""
+  if [[ -n "$1" ]]; then
+    domains="--domains=$1"
+  else
+    domains=""
+  fi
+  echo "$domains"
 }
 
 # main() {{{1
 function main() {
-    # We need a recursive level
-    rLevel=$(getRecursivityLevel "$cmdLevel")
-    # Do we target a domain
-    domainTarget=$(getDomainTarget "$cmdDomain")
-    # Test URL
-    declare -a urlArray
-    getLinkFromUrl "$cmdUrl"
+  # We need a recursive level
+  rLevel=$(getRecursivityLevel "$cmdLevel")
+  # Do we target a domain
+  domainTarget=$(getDomainTarget "$cmdDomain")
+  # Test URL
+  declare -a urlArray
+  getLinkFromUrl "$cmdUrl"
 
-    for i in "${urlArray[@]}"
-    do
-        echo "$i"
-    done
+  for i in "${urlArray[@]}"
+  do
+    echo "$i"
+  done
 }
 
 main
