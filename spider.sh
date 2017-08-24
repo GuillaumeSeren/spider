06447c562c3a73dd37aa393dd8d04d58423c1019
Guillaume Seren <guillaumeseren@gmail.com>
2017-08-24 19:15:45 +0200

    Refactor recursivity process POC
    The idea is to change from a long wget call,
    to an iterative per page call faster.
    
    The pros:
    * faster ressources discvery
    * we can display ressources per page easier
    
    The cons:
    * harder to manage the whole ressources list (duplicate)
    * more complexity in the code

--- spider.sh
+++ spider.sh
@@ -9,6 +9,11 @@
 # ---------------------------------------------
 
 # TaskList {{{1
+# @TODO:Add a urlTested to map all ressources already tested 1 time
+# @TODO Refactor the recursivity, use wget to target only 1 page
+#       then use an array to store the visited html ressources,
+#       then loop over the remaining html available links if more
+#       than 1 is given (also do not visit same url twice)
 # @TODO Add visited array to be sure to not go infinite in case of recursion
 # @TODO Add actual page that host the link
 # @TODO Add support for login (cookie / session)
@@ -163,20 +168,32 @@ function getOutputConfigured() {
   echo "$timeTask$urlProber$url"
 }
 
-# main() {{{1
+# function main() {{{1
 function main() {
-  declare -a urlArray
-  echo "testing: $cmdUrl"
+  declare -a urlActualRessources
+  declare -a urlVisited
+  declare -a urlUnVisited
+  declare -a urlUnVisitedOutput
+  # Add cmdUrl to urlUnVisited
+  urlUnVisited=(${urlUnVisited[@]} ${cmdUrl})
+  echo "testing: ${urlUnVisited[0]}"
+  # @FIXME Move that message to a debug / verbose mode
   echo "call web on: ${args}"
-  urlArray=($(bash ./web.sh -u "$cmdUrl" -l "$cmdLevel" -d "$cmdDomain"  ))
-  echo "call prober on array: ${#urlArray[@]}"
-
-  echo "time:http state:URL"
-  for i in "${urlArray[@]}"
+  # Now cmdLevel is not given to wget call but the number of loop below
+  urlActualRessources=($(bash ./web.sh -u "$cmdUrl" -l "1" -d "$cmdDomain"  ))
+  echo "Number of ressources of the page: ${#urlActualRessources[@]}"
+  # @FIXME Move that message to a debug / verbose mode
+  echo "time:page state:URL"
+  # This loop call prober (curl on each ressources of urlActualRessources)
+  for i in "${urlActualRessources[@]}"
   do
-    timeBegin="$(date +%s.%N)"
+    local timeBegin
+    local timeEnd
+    local timeTask
     local urlProber
-    if [[ ! -z "$cmdFilter" ]]; then
+    local urlHttp
+    timeBegin="$(date +%s.%N)"
+    if [[ -n "$cmdFilter" ]]; then
       urlProber="$(bash ./prober.sh -m 'content-type' -u "$i")"
     else
       urlProber="$(bash ./prober.sh -m 'http' -u "$i")"
@@ -190,7 +207,116 @@ function main() {
     elif [[ -z "$cmdFilter" && "$cmdFilter" == '' ]]; then
       getOutputConfigured "$timeTask" "$urlProber" "$i"
     fi
+    if [[ -n "$cmdFilter" ]]; then
+      urlHttp="${urlProber}"
+    else
+      urlHttp="$(bash ./prober.sh -m 'content-type' -u "$i")"
+      # echo 'cmdFilter is null'
+      # # echo "${urlProber}"
+      # echo "${urlHttp}"
+    fi
+    # set -x
+    if [[ "${urlHttp}" == "html" ]]; then
+      # if not already in the array we add it
+      # case "${myarray[@]}" in  "${urlHttp}") echo "found" ;; esac
+      # Add it to unvisited array
+      local item
+      local found
+      found=0
+      for item in "${urlUnVisited[@]}"
+      do
+        if [[ "${item}" == "${i}" ]]; then
+          found=1
+        fi
+      done
+      if [[ "${found}" == "0" ]]; then
+        # echo "add to the unvisited array $i"
+        urlUnVisited=(${urlUnVisited[@]} "${i}")
+
+      #   echo "was already in the array"
+      #   echo "${i}"
+      fi
+
+    # else
+    #   echo "is not a url"
+    #   echo "> ${urlHttp}"
+    fi
   done
+  # Add it to the visited
+  # urlVisited=(${urlVisited[@]} ${urlUnVisited[0]})
+  urlVisited=(${urlVisited[@]} ${cmdUrl})
+  # remove the visited from unvisited
+  local visited
+  local unVisited
+  local unVisitedId
+  local unVisitedMatch
+  echo 'clean the visited from the unVisited list'
+  for visited in "${urlVisited[@]}"
+  do
+    unVisitedId=0
+    for unVisited in "${urlUnVisited[@]}"
+    do
+      if [[ "${visited}" == "${unVisited}" ]]; then
+        echo "there is a match on ${#unVisited[@]}"
+        echo "${visited}"
+        # the is a match
+        # unVisitedMatch="${#unVisited[@]}"
+        unVisitedMatch="${unVisitedId}"
+        echo "unset ${unVisitedMatch}"
+        unset urlUnVisited["${unVisitedMatch}"]
+      fi
+      unVisitedId=$((unVisitedId+1))
+      # echo "unVisitedId = ${unVisitedId}"
+    done
+    # delete the match
+    # echo ">>>> ${unVisitedMatch}"
+  done
+  # Remove the unvisited url for the unvisited array
+  # @FIXME check if we delete the right think (validate the string)
+  # unset urlUnVisited[0]
+  # Display visited
+  echo 'visited'
+  for j in "${urlVisited[@]}"
+  do
+    echo "$j"
+  done
+  # Display unvisited
+  echo 'unvisited'
+  for k in  "${urlUnVisited[@]}"
+  do
+    # echo "${#urlUnVisited[@]} - $k"
+    # Re index as a new array
+    urlUnVisitedOutput=(${urlUnVisitedOutput[@]} ${k})
+  done
+  # echo "unvisitedOutput"
+  # for a in  "${urlUnVisitedOutput[@]}"
+  # do
+  #   # echo "${#urlUnVisitedOutput[@]} - $a"
+  #   # Re index as a new array
+  #   urlUnVisitedOutput=(${urlUnVisitedOutput[@]} ${a})
+  # done
+
+  # As the array was cleaned from some entries we need to re index
+  unset urlUnVisited
+  declare -a urlUnVisited
+  urlUnVisited=("${urlUnVisitedOutput[@]}")
+
+  # END Single pass
+  # Do we need to continue iterating ?
+
+  # basically just compare recursive parm if given with size of visited
+  if [[ "${cmdLevel}" -gt "${#urlVisited[@]}" || "${cmdLevel}" == 0 ]]; then
+    echo "let's have a look at the next in unVisited"
+    # echo ">>[0] ${urlUnVisitedOutput[0]}"
+    # echo ">>[1] ${urlUnVisitedOutput[1]}"
+    echo ">>[0] ${urlUnVisited[0]}"
+    # echo ">>[1] ${urlUnVisited[1]}"
+    # 1 check if not already in visited (might be a default)
+    # 2 send web to get the urlActualRessources array
+    # 3 loop on the ressources and probe the state
+    # 4 display to the user
+    # 5 Add the html in unVisited if not already in there
+  fi
 }
 
 main
